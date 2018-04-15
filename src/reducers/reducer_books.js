51852ddc02c9f5e490e1267b0973051bbe879240
Rajesh Babu <sammydave.e@gmail.com>
2018-04-15 18:55:47 +0200

    Conflict creation

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function() {
     return [
-        {'title': 'DSP processor and next level things'},
+        {'title': 'DSP processor and next level things'}, // I have made some changes here to make conflicts
         {'title': 'SSP and sales'},
         {'title': 'Account management'},
         {'title': 'Campaign management'}
